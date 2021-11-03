72fb3e8c99a4eadcb13e42a9f809e36ee679d4f3
carolbarbosa101 <cmp.1a.caroline@gmail.com>
2021-11-03 18:09:16 -0300

    salvando fim de tarde

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,23 +1,23 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
-
 import '../styles/repositories.scss';
-
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/carolbarbosa101'
-}
-
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+ 
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+  }, []);
+
   return (
     <section className="repository-list">
       <h1> Lista de repositórios</h1>
 
       <ul>
-      <RepositoryItem repository={repository} />
-      <RepositoryItem repository={repository} />
-      <RepositoryItem repository={repository} />
-      <RepositoryItem repository={repository} />
+      {repositories.map(repository => {
+        return  <RepositoryItem key={repository.name} repository={repository} />
+      })}
       </ul>
     </section>
   );
